c94a9b09a3b88808ff3047b4226f33cbfe3d1f85
Sarah Ayman <sayman@sumerge.com>
2022-08-08 14:57:39 +0200

    update img name + page reload

--- fe-task2/controller.js
+++ fe-task2/controller.js
@@ -2,30 +2,30 @@ var turn = 0;
 
 function change_icon(clicked_id) {
     if (turn == 0) {
-        document.getElementById(clicked_id).src = "x.png";
+        document.getElementById(clicked_id).src = "images/x.png";
+        document.getElementById(clicked_id).name = 'x';
         document.getElementById(clicked_id).disabled = true;
-        document.getElementById(clicked_id).alt = 'x';
 		turn = 1;
 	}
     else {
-		document.getElementById(clicked_id).src = "o.png";;
+		document.getElementById(clicked_id).src = "images/o.png";
+        document.getElementById(clicked_id).name = 'o';
 		document.getElementById(clicked_id).disabled = true;
-        document.getElementById(clicked_id).alt = 'o';
 		turn = 0;
 	}
 }
 
 function check() {
     var b1, b2, b3, b4, b5, b6, b7, b8, b9;
-	b1 = document.getElementById("b1").alt;
-	b2 = document.getElementById("b2").alt;
-	b3 = document.getElementById("b3").alt;
-	b4 = document.getElementById("b4").alt;
-    b5 = document.getElementById("b5").alt;
-	b6 = document.getElementById("b6").alt;
-	b7 = document.getElementById("b7").alt;
-	b8 = document.getElementById("b8").alt;
-	b9 = document.getElementById("b9").alt;
+	b1 = document.getElementById("b1").name;
+	b2 = document.getElementById("b2").name;
+	b3 = document.getElementById("b3").name;
+	b4 = document.getElementById("b4").name;
+    b5 = document.getElementById("b5").name;
+	b6 = document.getElementById("b6").name;
+	b7 = document.getElementById("b7").name;
+	b8 = document.getElementById("b8").name;
+	b9 = document.getElementById("b9").name;
 
     if ((b1 == 'x' && b2 == 'x' && b3 == 'x') || 
     (b4 == 'x' && b5 == 'x' && b6 == 'x') || (b7 == 'x' && b8 == 'x' && b9 == 'x') ||
@@ -38,6 +38,10 @@ function check() {
         }
 
         document.getElementById('Xwon').style.display = "block";
+
+        setTimeout(() => {
+            location.reload();
+        }, 4000);
 	}
 
     if ((b1 == 'o' && b2 == 'o' && b3 == 'o') || 
@@ -51,5 +55,9 @@ function check() {
         }
 
         document.getElementById('Owon').style.display = "block";
+        
+        setTimeout(() => {
+            location.reload();
+        }, 4000);
     }
 }
